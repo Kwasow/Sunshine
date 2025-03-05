eb8f036aa2c519919325c7477d114cd73cc86bab
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-05 11:57:13 +0100

    Fixed incorrect paths

--- backend/api/memories/get.php
+++ backend/api/memories/get.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__.'/../../config/config.php';
 require_once __DIR__.'/../../src/entities/memory.php';
 require_once __DIR__.'/../../src/helpers/authorization.php';
 require_once __DIR__.'/../../src/helpers/database.php';
--- backend/src/helpers/authorization.php
+++ backend/src/helpers/authorization.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__.'/../entities/user.php';
-require_once __DIR__.'/../helpers/firebase.php';
+require_once __DIR__.'/../../src/entities/user.php';
+require_once __DIR__.'/../../src/helpers/firebase.php';
 
 use Kreait\Firebase\Exception\Auth\FailedToVerifyToken;
 
