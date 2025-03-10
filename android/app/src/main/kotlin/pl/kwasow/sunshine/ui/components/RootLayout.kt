fedc5dd46bbff672195e97aacc77a5f617ce0629
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-10 18:40:54 +0100

    Rebuild layout with boxes

--- android/app/src/main/kotlin/pl/kwasow/sunshine/ui/components/RootLayout.kt
+++ android/app/src/main/kotlin/pl/kwasow/sunshine/ui/components/RootLayout.kt
@@ -1,15 +1,28 @@
 package pl.kwasow.sunshine.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 // ====== Public composables
 @Composable
 fun RootLayout(
-    modifier: Modifier = Modifier.fillMaxSize(),
     bottomBar: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
+    Box {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            content()
+        }
 
+        Box(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            bottomBar()
+        }
+    }
 }
