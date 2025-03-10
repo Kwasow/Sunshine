d2d1d0b277e45633c90328ee7111a6b17f54eba0
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-10 17:11:17 +0100

    Migrate to version catalog (#20)
    Closes #16

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("android") version "2.1.10" apply false
-    kotlin("plugin.serialization") version "2.1.10" apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 
-    id("com.android.application") version "8.8.2" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.mapsplatform) apply false
+    alias(libs.plugins.google.services) apply false
 }
