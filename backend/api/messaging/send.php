32073d3877bfacea0aad27b1d99212a901fc02f4
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-10 10:58:32 +0100

    Moved message sending logic

--- backend/api/messaging/send.php
+++ backend/api/messaging/send.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__.'/../../src/helpers/authorization.php';
 require_once __DIR__.'/../../src/helpers/database.php';
+require_once __DIR__.'/../../src/helpers/firebase.php';
 
 use Kreait\Firebase\Exception\MessagingException;
 use Kreait\Firebase\Messaging\CloudMessage;
@@ -25,43 +26,38 @@ $postData = json_decode(file_get_contents('php://input'), true);
 $type = $postData['type'];
 
 // Send message
-$messaging = $GLOBALS['firebaseMessaging'];
-$message = null;
+$topic = null;
+$data = null;
 
 switch ($type) {
     case 'missing_you':
         $topic = $user->getMissingYouRecipient()->getUserTopic();
         $data = [
-        'type' => 'missing_you',
-        'name' => $user->getFirstName()
+            'type' => 'missing_you',
+            'name' => $user->getFirstName()
         ];
-
-        $message = CloudMessage::withTarget('topic', $topic)
-        ->withData($data);
         break;
     case 'request_location':
         $topic = $user->getMissingYouRecipient()->getUserTopic();
         $data = [
         'type' => 'request_location'
         ];
-
-        $message = CloudMessage::withTarget('topic', $topic)
-        ->withData($data);
         break;
     default:
         break;
 }
 
-if ($message == null) {
-    http_response_code(400);
-    error_log("[ERROR] Unknown message type: " . $type);
-} else {
-    try {
-        $result = $messaging->send($message);
-    } catch (MessagingException $e) {
-        http_response_code(500);
-        error_log("[EXCEPTION]: " . $e->getMessage());
+try {
+    $result = sendTopicFirebaseMessage($topic, $data);
+
+    if ($result == null) {
+        http_response_code(400);
+        error_log("[ERROR] Error handling message type: " . $type);
     }
+} catch (Exception $e) {
+    http_response_code(500);
+    error_log("[ERROR] Error handling message type: " . $type);
+    error_log("[EXCEPTION]: " . $e->getMessage());
 }
 
 mysqli_close($conn);
