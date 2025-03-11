932bdabbf05f9952896766b00a581d1e26f6b409
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-11 15:13:34 +0100

    Implemented full screen photo view

--- android/app/src/main/kotlin/pl/kwasow/sunshine/ui/screens/photo/PhotoScreen.kt
+++ android/app/src/main/kotlin/pl/kwasow/sunshine/ui/screens/photo/PhotoScreen.kt
@@ -1,15 +1,25 @@
 package pl.kwasow.sunshine.ui.screens.photo
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import pl.kwasow.sunshine.ui.components.PhotoView
 
 // ====== Public composables
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun PhotoPreviewScreen(uri: String) {
-    Surface(
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-    ) {
+        containerColor = Color.Black,
+    ) { paddingValues ->
+        PhotoView(
+            modifier = Modifier.fillMaxSize(),
+            uri = uri,
+            clickable = false,
+        )
     }
 }
